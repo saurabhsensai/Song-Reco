94ec785165eda7cd145058cbdaa260184d212a36
Saurabh Nale <115738650+saurabhsensai@users.noreply.github.com>
2025-01-09 22:18:03 +0530

    Update script.py

--- script.py
+++ script.py
@@ -18,6 +18,7 @@ class InstagramAudioExtractor:
         ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
+
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
